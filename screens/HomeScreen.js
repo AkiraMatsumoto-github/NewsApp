726c30723cff9bffdf5349d165f3c40fae630461
AkiraMatsumoto <95224877+AkiraMatsumoto-github@users.noreply.github.com>
2022-01-28 14:27:24 +0900

    Session9

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet, FlatList, SafeAreaView } from "react-native";
 import ListItem from "../components/ListItem";
+import Loading from "../components/Loading";
 import Constants from "expo-constants";
 import axios from "axios";
 
@@ -15,18 +16,21 @@ const URL = `https://newsapi.org/v2/top-headlines?country=jp&category=business&a
 
 export default HomeScreen = ({ navigation }) => {
   const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     fetchArticles();
   }, []);
 
   const fetchArticles = async () => {
+    setLoading(true);
     try {
       const response = await axios.get(URL);
       setArticles(response.data.articles);
     } catch (error) {
       console.error(error);
     }
+    setLoading(false);
   };
 
   return (
@@ -43,6 +47,7 @@ export default HomeScreen = ({ navigation }) => {
         )}
         keyExtractor={(item, index) => index.toString()}
       />
+      {loading && <Loading />}
     </SafeAreaView>
   );
 };
